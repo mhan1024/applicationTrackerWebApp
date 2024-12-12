8752383a4465c2b4620f80bba81853e3c79bce33
Michelle Han <mhan1024@umd.edu>
2024-12-12 14:26:54 -0500

    fixed create account bug and removed .trim for render

--- server.js
+++ server.js
@@ -614,7 +614,7 @@ process.stdout.write(prompt);
 process.stdin.on("readable", function () {
     const userInput = process.stdin.read();
 
-    if (userInput.trim() === "stop") {
+    if (userInput === "stop") {
         console.log("Shutting down the server");
         process.exit(0);
 
